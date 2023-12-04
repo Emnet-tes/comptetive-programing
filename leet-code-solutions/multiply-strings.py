c97cb4570d58bdb205266ec3b277232c5edd3ad3
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2023-12-05 02:31:31 +0300

    Added solution for Multiply Strings

--- leet-code-solutions/multiply-strings.py
+++ leet-code-solutions/multiply-strings.py
@@ -1,4 +1,9 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
-        
+        r1=0
+        r2=0
+        for i in range(len(num1)):
+            r1=r1*10+ord(num1[i])-48
+        for i in range(len(num2)):
+            r2=r2*10+ord(num2[i])-48
+        return str(r1*r2)
