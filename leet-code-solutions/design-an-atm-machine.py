efab05f85ffdb9db4c675ffc8f47083d4e0763cb
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2023-12-20 03:47:38 +0300

    Added solution for Design an ATM Machine

---
+++ leet-code-solutions/design-an-atm-machine.py
@@ -0,0 +1,28 @@
+class ATM:
+
+    def __init__(self):
+        self.box = [0] * 5
+        self.values = [20, 50, 100, 200, 500]
+
+    def deposit(self, banknotes_count: List[int]) -> None:
+        for i, n in enumerate(banknotes_count):
+            self.box[i] += n
+
+    def withdraw(self, amount: int) -> List[int]:
+        res = []
+        for val, n in zip(self.values[::-1], self.box[::-1]):
+            need = min(n, amount // val)
+            res = [need] + res
+            amount -= (need * val)
+        if amount == 0:
+            self.deposit([-x for x in res])
+            return res
+        else:
+            return [-1]
+        
+
+
+# Your ATM object will be instantiated and called as such:
+# obj = ATM()
+# obj.deposit(banknotesCount)
+# param_2 = obj.withdraw(amount)
