37e8ed4b43f7ae8f79371e6cd55e2dca30987389
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2023-12-27 14:41:42 +0300

    Added solution for Sort the People

--- leet-code-solutions/sort-the-people.py
+++ leet-code-solutions/sort-the-people.py
@@ -1,19 +1,18 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        mydect=defaultdict(int)
+        #using bubble sort algorithm
+        box=defaultdict(int)
+
         for i in range(len(heights)):
-            mydect[heights[i]]=names[i]
-        for i in range(len(heights)):
-            for j in range(len(heights)-1 ):
-                if heights[j]< heights[j+1]:
-                    temp=heights[j]
-                    heights[j]=heights[j+1]
-                    heights[j+1]=temp
+            box[heights[i]]=names[i]
+
+        for i in range(len(heights)-1,0,-1):
+            for j in range(0,i):
+                if heights[j+1]>heights[j]:
+                    heights[j+1],heights[j] = heights[j],heights[j+1]
+
         li=[]
         for i in range(len(heights)):
-            li.append(mydect[heights[i]])
-
-        return li
+            li.append(box[heights[i]])
             
-                
-        
+        return li
