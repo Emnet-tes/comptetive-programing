7a1580ca52e7113c556a765664538ca0ef811c9f
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2024-01-02 16:31:33 +0300

    Added solution for Pancake Sorting

---
+++ leet-code-solutions/pancake-sorting.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        end=len(arr)
+        res=[]
+        while end>1:
+            maxInd=arr.index(end) 
+            if maxInd==end-1: 
+                end-=1
+                continue
+
+            if maxInd!=0:
+                arr[:maxInd+1]=reversed(arr[:maxInd+1])
+                res.append(maxInd+1) 
+                
+                
+            arr[:end]=reversed(arr[:end])
+            res.append(end)
+            
+            end-=1 
+        return res
