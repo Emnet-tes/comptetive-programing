edb88585c06d704ae4330b8392aae11b25e83484
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2024-02-18 15:43:52 +0300

    Add solution for Separate Black and White Balls

--- leet-code-solutions/leetcode/separate-black-and-white-balls.py
+++ leet-code-solutions/leetcode/separate-black-and-white-balls.py
@@ -1,11 +1,10 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        ans = 0
-        cnt = 0
-        for c in reversed(s):
-            if c == '0':
-                cnt += 1
+        cnt=0
+        ans=0
+        for c in s:
+            if c=='1':
+                cnt+=1
             else:
-                ans += cnt
-            
+                ans+=cnt
         return ans
