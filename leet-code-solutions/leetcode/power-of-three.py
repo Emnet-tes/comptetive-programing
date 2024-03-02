56667ad9b37ee8627cca96e640a79ca0a9ed48eb
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2024-03-02 19:06:42 +0300

    Add solution for Power of Three

--- leet-code-solutions/leetcode/power-of-three.py
+++ leet-code-solutions/leetcode/power-of-three.py
@@ -1,13 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        num=1
-        def reoccur(n,num,i):
-            if num==n:
-                return True
-            if num>n:
-                return False
-            num=pow(3,i)
-            print(num)
-            return reoccur(n,num,i+1)
-        return reoccur(n,num,0)
+        if n == 0 : return False
+        if n == 1 : return True
+        return self.isPowerOfThree(n/3)
+        
         
