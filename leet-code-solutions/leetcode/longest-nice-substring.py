8bd3ae1cd61e15d9994b674bc2dd10c50d5e41fb
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2024-03-11 19:14:08 +0300

    Add solution for Longest Nice Substring

--- leet-code-solutions/leetcode/longest-nice-substring.py
+++ leet-code-solutions/leetcode/longest-nice-substring.py
@@ -1,20 +1,38 @@
+
 class Solution:
-    def longestNiceSubstring(self, s: str) -> str:
+    def longestNiceSubstring (self, s: str) -> str:
+
         n = len(s)
+
         mx = 0
+
+        
         k = -1
-        for i in range(n):
+
+        for i in range(len(s)):
+
             lower = 0
+
             upper = 0
+
             for j in range(i, n):
+
                 c = s[j]
+
                 if c.islower():
-                    lower |= 1 << (ord(c) - ord('a'))
+
+                    lower |= 1 << (ord(c) -ord('a'))
+
                 else:
+
                     upper |= 1 << (ord(c) - ord('A'))
+
                 if lower == upper and mx < j - i + 1:
+
                     mx = j - i + 1
+
                     k = i
+
         return "" if k == -1 else s[k:k+mx]
 
-            
+          
