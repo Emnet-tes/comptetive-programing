e0263e62c9643f7463b974d320ad9416af8f48a8
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2024-03-19 23:21:49 +0300

    Add solution for Find Right Interval

--- leet-code-solutions/leetcode/find-right-interval.py
+++ leet-code-solutions/leetcode/find-right-interval.py
@@ -1,16 +1,13 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        k = len(intervals)
-        ans=[-1]*len(intervals)
-        start_index={}
+        size = len(intervals)
+        start_idx = defaultdict(int)
         for i,n in enumerate(intervals):
-            start_index[n[0]]=i
+            start_idx[n[0]] = i
         sorted_intervals=sorted(intervals , key = lambda x : x[0])
-
+        ans = [-1]*size
         for i,n in enumerate(intervals):
-            postion=bisect_left(sorted_intervals,[n[1],-float('inf')])
-
-            if postion != k:
-                ans[i]=start_index[sorted_intervals[postion][0]]
+            postion = bisect_left(sorted_intervals,[n[1],float('-inf')])
+            if postion != size:
+                ans[i]=start_idx[sorted_intervals[postion][0]]
         return ans
-        
