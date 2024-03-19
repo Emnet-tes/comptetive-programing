82c25817decdd2b25c85b7e5f8db24130f6c6602
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2024-03-19 11:45:57 +0300

    Add solution for Range Sum Query 2D - Immutable

--- leet-code-solutions/leetcode/range-sum-query-2d-immutable.py
+++ leet-code-solutions/leetcode/range-sum-query-2d-immutable.py
@@ -2,26 +2,18 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         self.matrix=matrix
-        self.grid=[[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
-        for r in range(len(self.matrix)):
-            for c in range(len(self.matrix[0])):
-                self.grid[r][c] = self.matrix[r][c]
-                if r > 0:
-                    self.grid[r][c] += self.grid[r - 1][c]
-                if c > 0:
-                    self.grid[r][c] += self.grid[r][c - 1]
-                if c > 0 and r > 0:
-                    self.grid[r][c] -= self.grid[r - 1][c - 1]
+        row=len(matrix)
+        col=len(matrix[0])
+        self.grid=[ [0 for _ in range(col+1)]  for _ in range(row+1)] 
 
+        for r in range(1,row+1):
+            for c in range(1,col+1):
+
+                self.grid[r][c]=self.matrix[r-1][c-1]+self.grid[r-1][c]+self.grid[r][c-1]-self.grid[r-1][c-1]
+                
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        total = self.grid[row2][col2]
-        if row1 > 0:
-            total -= self.grid[row1 - 1][col2]
-        if col1 > 0:
-            total -= self.grid[row2][col1 - 1]
-        if row1 > 0 and col1 > 0:
-            total += self.grid[row1 - 1][col1 - 1]
-        return total
+        return self.grid[row2+1][col2+1]-self.grid[row1][col2+1]-self.grid[row2+1][col1]+self.grid[row1][col1]
+        
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
